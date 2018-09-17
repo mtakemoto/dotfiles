3f01870d1b83aa3ad6e9123d70cd19309c5109fe
Matt Takemoto <Matt.Takemoto@lumedx.com>
2018-09-17 15:14:20 -0700

    adding aliases to powershell prompt

--- Profile.ps1
+++ Profile.ps1
@@ -3,6 +3,22 @@ Import-Module -Name posh-git
 #--Enable when I need the SSH Agent
 #Start-SshAgent
 
+#Aliases
+function git_status_alias() {
+    git status
+}
+
+function git_add_modified_alias() {
+    git add -u
+}
+
+function git_push_curr_branch_alias() {
+    git push -u origin ${git rev-parse --abbrev-ref HEAD}
+}
+
+New-Alias -Name gs -Value git_status_alias
+New-Alias -Name gau -Value git_add_modified_alias
+New-Alias -Name gpcb -Value git_push_curr_branch_alias
 
 function Test-Administrator {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
@@ -15,7 +31,7 @@ function prompt {
     Write-Host
 
     # Reset color, which can be messed up by Enable-GitColors
-    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+    # $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
     if (Test-Administrator) {  # Use different username if elevated
         Write-Host "(Elevated) " -NoNewline -ForegroundColor White
