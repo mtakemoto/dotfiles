f68e66258aff3cef55aaf2d32de8396dfee34056
Matthew Takemoto <1422569-mtakemoto@users.noreply.gitlab.com>
2022-04-26 16:59:43 -0700

    Maybe the install script works now?

--- install.sh
+++ install.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-##TODO: use a map to for file -> link for single ln with params
-##TODO: install ZSH
-##TODO: install Oh-my-zsh
-##TODO: install nvim
-##TODO: install lambda-mod zsh theme
-##TODO: after all of the above, then copy files so ZSH theme applies
-##TODO: nvim plugin install
-##TODO: install powerline font (cascadia cove powerline patch from nerdfonts currently)
+bold=$(tput bold)
+normal=$(tput sgr0)
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DIRNAME="$(dirname "$BASH_SOURCE")"
-
-## Install prereqs
-#sudo apt-get update
-#sudo apt-get install zsh neovim tmux tree httpie
-
-## Link NeoVim config first
-mkdir -p ~/.config/nvim
-ln -svf "$DIR/.config/nvim/init.vim" ~/.config/nvim/init.vim
+PACKAGE_LIST="zsh tmux neovim tree httpie"
+CUSTOM_FONT_DIR=/usr/share/fonts/truetype/customfonts
 
 ## List of files to symlink in home dir
 declare -a files=(".bashrc"
@@ -28,6 +16,33 @@ declare -a files=(".bashrc"
                   ".zshrc"
                   )
 
+## -----------------------
+## Bootstrap
+echo "${bold}Bootstrapping...${normal}"
+sudo apt-get update
+sudo apt-get install curl wget aptitude git software-properties-common
+
+## Package repositories
+echo "${bold}Adding repositories...${normal}"
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+
+## Install packages
+# sudo apt-get install -y git zsh tmux tree httpie
+echo "${bold}Installing packages...${normal}"
+sudo aptitude install -y $PACKAGE_LIST
+
+## Install oh-my-zsh
+echo "${bold}Installing Oh-my-zsh...${bold}"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+## Link NeoVim config first
+echo "${bold}Linking config files...${bold}"
+mkdir -p ~/.config/nvim
+ln -svf "$DIR/.config/nvim/init.vim" ~/.config/nvim/init.vim
+
+
 ## Symlink files to destination
 ## Prompt for each if removal is needed
 for i in "${files[@]}"
@@ -36,6 +51,7 @@ do
 done
 
 ## Copy theme
+echo "Copying theme"
 cp -v "$DIR/lambda-mod.zsh-theme" ~/.oh-my-zsh/themes/lambda-mod.zsh-theme
 
 ## Enable Git Credential Manager
@@ -47,3 +63,14 @@ select yn in "Yes" "No"; do
     esac
 done
 
+## Neovim setup
+echo "${bold}Installing neovim plugins....${normal}"
+neovim +PlugInstall +qall
+
+## Fonts
+echo "${bold}Installing fonts...${normal}"
+sudo mkdir $CUSTOM_FONT_DIR
+sudo cp "$DIR/fonts/*.ttf" $CUSTOM_FONT_DIR
+sudo fc-cache -fv
+
+echo ${bold}Done! :D${normal}"
